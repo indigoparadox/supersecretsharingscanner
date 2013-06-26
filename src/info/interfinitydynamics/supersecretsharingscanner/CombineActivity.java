dde49b3cf4f362bb9ee2e42c8bb1e3bc426aaa22
indigoparadox <indigoparadox+bitbucket@interfinitdynamics.info>
2013-06-26 11:53:45 -0400

    Changed the interface to use a spinner for entered shares. The selected share still doesn't show, though.

--- src/info/interfinitydynamics/supersecretsharingscanner/CombineActivity.java
+++ src/info/interfinitydynamics/supersecretsharingscanner/CombineActivity.java
@@ -3,6 +3,7 @@ package info.interfinitydynamics.supersecretsharingscanner;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,43 +13,174 @@ import com.tiemens.secretshare.main.cli.MainCombine.CombineInput;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.database.DataSetObserver;
+import android.graphics.Color;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class CombineActivity extends Activity {
+   
+   class ShareData {
+      protected int cIntShareNum;
+      protected String cStrShareString;
+      
+      public ShareData( int intShareNumIn, String strShareStringIn ) {
+         cIntShareNum = intShareNumIn;
+         cStrShareString = strShareStringIn;
+      }
+      
+      public String toString() {
+         return String.format( "%d - %s", cIntShareNum, cStrShareString );
+      }
+   }
+   
+   class ShareAdapter implements SpinnerAdapter {
+      ArrayList<ShareData> caShrShareData;
+      
+      public ShareAdapter( ArrayList<ShareData> aShrShareDataIn ) {
+         caShrShareData = aShrShareDataIn;
+      }
+
+      @Override
+      public int getCount() {
+         return caShrShareData.size();
+      }
+
+      @Override
+      public Object getItem( int intPositionIn ) {
+         return caShrShareData.get( intPositionIn );
+      }
+
+      @Override
+      public long getItemId( int intPositionIn ) {
+         return intPositionIn;
+      }
+
+      @Override
+      public int getItemViewType( int intPositionIn ) {
+         return android.R.layout.simple_spinner_dropdown_item;
+      }
+
+      @Override
+      public View getView(
+         int intPositionIn,
+         View viwConvertViewIn,
+         ViewGroup vwgParentIn
+      ) {
+         TextView txtView = new TextView( getApplicationContext() );
+         txtView.setTextColor( Color.BLACK );
+         txtView.setText( caShrShareData.get( intPositionIn ).toString() );
+         return txtView; 
+      }
+
+      @Override
+      public int getViewTypeCount() {
+         return 1;
+      }
+
+      @Override
+      public boolean hasStableIds() {
+         return false;
+      }
+
+      @Override
+      public boolean isEmpty() {
+         return false;
+      }
+
+      @Override
+      public void registerDataSetObserver( DataSetObserver dsoObserverIn ) {
+         // TODO Auto-generated method stub
+      }
+
+      @Override
+      public void unregisterDataSetObserver( DataSetObserver dsoObserverIn ) {
+         // TODO Auto-generated method stub
+      }
+
+      @Override
+      public View getDropDownView(
+         int intPositionIn,
+         View viwConvertViewIn,
+         ViewGroup vwgParentIn
+      ) {
+         return this.getView( intPositionIn, viwConvertViewIn, vwgParentIn );
+      }
+   }
+   
    // Test string: "the number of shares to generate"
-   private String[] caShareArray = {
+   /* private String[] caShareArray = {
       "-k", "3",
       "-s1", "bigintcs:007468-65206e-756d62-657220-6f6620-736875-dfbe54-c94a99-ad0896-03dd4e-316518-77FA28",
       "-s3", "bigintcs:007468-65206e-756d62-657220-6f6620-736902-f59561-08d2f1-68912f-2f9a6a-a5c4c4-18E0E9",
       "-s4", "bigintcs:007468-65206e-756d62-657220-6f6620-73697b-9e138b-9f851e-977897-c5dfab-4a33bd-03161E",
-   };
+   }; */
+   
+   protected ArrayList<ShareData> caShrShareList = new ArrayList<ShareData>(); 
 
    @Override
    protected void onCreate( Bundle savedInstanceState ) {
       super.onCreate( savedInstanceState );
       setContentView( R.layout.activity_combine );
-      
+            
       final Button btnAddShare = (Button)findViewById( R.id.btnAddShare );
       btnAddShare.setOnClickListener( new View.OnClickListener() {
           public void onClick(View v) {
              // TODO: Check specified share for validity.
              
-             // TODO: Figure out some kind of list view layout so that a share
-             //       can be selected and deleted.
-             
-             
+             final EditText txtShareNum =
+                (EditText)findViewById( R.id.txtShareNum );
+             final EditText txtShareString =
+                (EditText)findViewById( R.id.txtShareString );
+             try {
+                addShare(
+                   Integer.parseInt( txtShareNum.getText().toString() ),
+                   txtShareString.getText().toString()
+                );
+             } catch( NumberFormatException ex ) {
+                // TODO: Warn about using a numerical share ID.
+                return;
+             }
           }
       } );
       
       final Button btnCombine = (Button)findViewById( R.id.btnCombine );
       btnCombine.setOnClickListener( new View.OnClickListener() {
           public void onClick(View v) {
-             combineTAShares();
+             combineTAShares( caShrShareList );
           }
+      } );
+
+      // Populate the share spinner.
+      final Spinner spnShares = (Spinner)findViewById( R.id.spnShares );
+      ShareAdapter adpShareAdapter = new ShareAdapter( caShrShareList );
+      spnShares.setAdapter( adpShareAdapter );
+      spnShares.setOnItemSelectedListener( new OnItemSelectedListener() {
+
+         @Override
+         public void onItemSelected(
+            AdapterView<?> advParentIn,
+            View viwViewIn,
+            int intPositionIn,
+            long lngIDIn
+         ) {
+            //ShareData shrSelected = 
+            //   (ShareData)advParentIn.getItemAtPosition( intPositionIn );
+         }
+
+         @Override
+         public void onNothingSelected( AdapterView<?> advParentIn ) {
+         }
       } );
    }
 
@@ -58,10 +190,25 @@ public class CombineActivity extends Activity {
       getMenuInflater().inflate( R.menu.combine, menu );
       return true;
    }
+   
+   public void addShare( int intShareNumIn, String strShareStringIn ) {
+      caShrShareList.add( new ShareData( intShareNumIn, strShareStringIn ) );
+   }
 
-   public String combineTAShares() {
+   public String combineTAShares( ArrayList<ShareData> aShrShareDataIn ) {
       String strOutputSecret = null;
       
+      // Build a command line from the share list.
+      ArrayList<String> aStrCommandLine = new ArrayList<String>();
+      aStrCommandLine.add( "-k" );
+      aStrCommandLine.add( "3" );
+      
+      //"-s1", "bigintcs:007468-65206e-756d62-657220-6f6620-736875-dfbe54-c94a99-ad0896-03dd4e-316518-77FA28",
+      
+      for( ShareData shrIter : aShrShareDataIn ) {
+         aStrCommandLine.add( String.format( "-s%d", shrIter.cIntShareNum ) );
+         aStrCommandLine.add( shrIter.cStrShareString );
+      }
       //EditText txtShares = (EditText)findViewById( R.id.txtShares );
       
       //String[] aStrShares = txtShares.getText().toString().split( "[\\r\\n]+" );
@@ -72,7 +219,9 @@ public class CombineActivity extends Activity {
          PrintStream psOutput = new PrintStream( osOutput );
          
          // Combine the shares and print them to a string.
-         CombineInput ssssInput = CombineInput.parse( caShareArray );
+         String[] aStrCommandLineRO = new String[aStrCommandLine.size()];
+         aStrCommandLine.toArray( aStrCommandLineRO );
+         CombineInput ssssInput = CombineInput.parse( aStrCommandLineRO );
          CombineOutput ssssOutput = ssssInput.output();
          ssssOutput.print( psOutput );
          
@@ -91,9 +240,7 @@ public class CombineActivity extends Activity {
          }
          
          // Toast.makeText( this, strOutputSecret, Toast.LENGTH_LONG ).show();
-      } catch( SecretShareException ex ) {
-         Toast.makeText( this, ex.getMessage(), Toast.LENGTH_LONG ).show();
-      } catch( UnsupportedEncodingException ex ) {
+      } catch( Exception ex ) {
          Toast.makeText( this, ex.getMessage(), Toast.LENGTH_LONG ).show();
       }
       return strOutputSecret;
