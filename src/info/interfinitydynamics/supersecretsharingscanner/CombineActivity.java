5da280b19f8e52069680f5ee139bc5dcaf901050
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2013-06-26 13:15:54 -0400

    Switched to a list view, as this seems to be less infuriating to work with than a spinner.

--- src/info/interfinitydynamics/supersecretsharingscanner/CombineActivity.java
+++ src/info/interfinitydynamics/supersecretsharingscanner/CombineActivity.java
@@ -2,7 +2,6 @@ package info.interfinitydynamics.supersecretsharingscanner;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,22 +11,20 @@ import com.tiemens.secretshare.main.cli.MainCombine.CombineOutput;
 import com.tiemens.secretshare.main.cli.MainCombine.CombineInput;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.database.DataSetObserver;
 import android.graphics.Color;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+@SuppressLint("DefaultLocale")
 public class CombineActivity extends Activity {
    
    class ShareData {
@@ -41,80 +38,6 @@ public class CombineActivity extends Activity {
       
       public String toString() {
          return String.format( "%d - %s", cIntShareNum, cStrShareString );
-      }
-   }
-   
-   class ShareAdapter implements SpinnerAdapter {
-      ArrayList<ShareData> caShrShareData;
-      
-      public ShareAdapter( ArrayList<ShareData> aShrShareDataIn ) {
-         caShrShareData = aShrShareDataIn;
-      }
-
-      @Override
-      public int getCount() {
-         return caShrShareData.size();
-      }
-
-      @Override
-      public Object getItem( int intPositionIn ) {
-         return caShrShareData.get( intPositionIn );
-      }
-
-      @Override
-      public long getItemId( int intPositionIn ) {
-         return intPositionIn;
-      }
-
-      @Override
-      public int getItemViewType( int intPositionIn ) {
-         return android.R.layout.simple_spinner_dropdown_item;
-      }
-
-      @Override
-      public View getView(
-         int intPositionIn,
-         View viwConvertViewIn,
-         ViewGroup vwgParentIn
-      ) {
-         TextView txtView = new TextView( getApplicationContext() );
-         txtView.setTextColor( Color.BLACK );
-         txtView.setText( caShrShareData.get( intPositionIn ).toString() );
-         return txtView; 
-      }
-
-      @Override
-      public int getViewTypeCount() {
-         return 1;
-      }
-
-      @Override
-      public boolean hasStableIds() {
-         return false;
-      }
-
-      @Override
-      public boolean isEmpty() {
-         return false;
-      }
-
-      @Override
-      public void registerDataSetObserver( DataSetObserver dsoObserverIn ) {
-         // TODO Auto-generated method stub
-      }
-
-      @Override
-      public void unregisterDataSetObserver( DataSetObserver dsoObserverIn ) {
-         // TODO Auto-generated method stub
-      }
-
-      @Override
-      public View getDropDownView(
-         int intPositionIn,
-         View viwConvertViewIn,
-         ViewGroup vwgParentIn
-      ) {
-         return this.getView( intPositionIn, viwConvertViewIn, vwgParentIn );
       }
    }
    
@@ -161,27 +84,31 @@ public class CombineActivity extends Activity {
           }
       } );
 
-      // Populate the share spinner.
-      final Spinner spnShares = (Spinner)findViewById( R.id.spnShares );
-      ShareAdapter adpShareAdapter = new ShareAdapter( caShrShareList );
-      spnShares.setAdapter( adpShareAdapter );
-      spnShares.setOnItemSelectedListener( new OnItemSelectedListener() {
-
-         @Override
-         public void onItemSelected(
-            AdapterView<?> advParentIn,
-            View viwViewIn,
-            int intPositionIn,
-            long lngIDIn
+      // Populate the share listbox.
+      final ListView lstShares = (ListView)findViewById( R.id.lstShares );
+      ArrayAdapter<ShareData> adpShareAdapter = 
+         new ArrayAdapter<CombineActivity.ShareData>(
+            getApplicationContext(),
+            android.R.layout.simple_list_item_1,
+            caShrShareList
          ) {
-            //ShareData shrSelected = 
-            //   (ShareData)advParentIn.getItemAtPosition( intPositionIn );
-         }
 
-         @Override
-         public void onNothingSelected( AdapterView<?> advParentIn ) {
-         }
-      } );
+            @Override
+            public View getView(
+               int intPositionIn,
+               View viwConvert,
+               ViewGroup vwgParent
+            ) {
+                View viwView = super.getView(
+                   intPositionIn, viwConvert, vwgParent
+                );
+                TextView txtView =
+                   (TextView)viwView.findViewById( android.R.id.text1 );
+                txtView.setTextColor( Color.BLACK );
+                return viwView;
+            }
+         };
+      lstShares.setAdapter( adpShareAdapter );
    }
 
    @Override
@@ -191,8 +118,11 @@ public class CombineActivity extends Activity {
       return true;
    }
    
+   @SuppressWarnings("unchecked")
    public void addShare( int intShareNumIn, String strShareStringIn ) {
       caShrShareList.add( new ShareData( intShareNumIn, strShareStringIn ) );
+      final ListView lstShares = (ListView)findViewById( R.id.lstShares );
+      ((ArrayAdapter<ShareData>)lstShares.getAdapter()).notifyDataSetChanged();
    }
 
    public String combineTAShares( ArrayList<ShareData> aShrShareDataIn ) {
@@ -203,17 +133,11 @@ public class CombineActivity extends Activity {
       aStrCommandLine.add( "-k" );
       aStrCommandLine.add( "3" );
       
-      //"-s1", "bigintcs:007468-65206e-756d62-657220-6f6620-736875-dfbe54-c94a99-ad0896-03dd4e-316518-77FA28",
-      
       for( ShareData shrIter : aShrShareDataIn ) {
          aStrCommandLine.add( String.format( "-s%d", shrIter.cIntShareNum ) );
          aStrCommandLine.add( shrIter.cStrShareString );
       }
-      //EditText txtShares = (EditText)findViewById( R.id.txtShares );
       
-      //String[] aStrShares = txtShares.getText().toString().split( "[\\r\\n]+" );
-      
-      // TODO: Prune any arguments or non-shares from the text area.
       try {
          ByteArrayOutputStream osOutput = new ByteArrayOutputStream();
          PrintStream psOutput = new PrintStream( osOutput );
