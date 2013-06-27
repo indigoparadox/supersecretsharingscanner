3115f591bb9219571aebaaf99282faea2cb72102
indigoparadox <indigoparadox+bitbucket@interfinitdynamics.info>
2013-06-27 15:05:59 -0400

    Added rough ZXing support for scanning shares from QR codes.

--- src/info/interfinitydynamics/supersecretsharingscanner/CombineActivity.java
+++ src/info/interfinitydynamics/supersecretsharingscanner/CombineActivity.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.google.zxing.integration.android.IntentIntegrator;
 import com.tiemens.secretshare.exceptions.SecretShareException;
 import com.tiemens.secretshare.main.cli.MainCombine.CombineOutput;
 import com.tiemens.secretshare.main.cli.MainCombine.CombineInput;
@@ -13,6 +14,7 @@ import com.tiemens.secretshare.main.cli.MainCombine.CombineInput;
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.view.Menu;
 import android.view.View;
@@ -26,89 +28,96 @@ import android.widget.Toast;
 
 @SuppressLint("DefaultLocale")
 public class CombineActivity extends Activity {
-   
+
    class ShareData {
       protected int cIntShareNum;
       protected String cStrShareString;
-      
+
       public ShareData( int intShareNumIn, String strShareStringIn ) {
          // TODO: Check specified share for validity.
          cIntShareNum = intShareNumIn;
          cStrShareString = strShareStringIn;
       }
-      
+
       public String toString() {
          return String.format( "%d - %s", cIntShareNum, cStrShareString );
       }
    }
-   
+
    // Test string: "the number of shares to generate"
-   /* private String[] caShareArray = {
-      "-k", "3",
-      "-s1", "bigintcs:007468-65206e-756d62-657220-6f6620-736875-dfbe54-c94a99-ad0896-03dd4e-316518-77FA28",
-      "-s3", "bigintcs:007468-65206e-756d62-657220-6f6620-736902-f59561-08d2f1-68912f-2f9a6a-a5c4c4-18E0E9",
-      "-s4", "bigintcs:007468-65206e-756d62-657220-6f6620-73697b-9e138b-9f851e-977897-c5dfab-4a33bd-03161E",
-   }; */
-   
-   protected ArrayList<ShareData> caShrShareList = new ArrayList<ShareData>(); 
+   /*
+    * private String[] caShareArray = { "-k", "3", "-s1",
+    * "bigintcs:007468-65206e-756d62-657220-6f6620-736875-dfbe54-c94a99-ad0896-03dd4e-316518-77FA28"
+    * , "-s3",
+    * "bigintcs:007468-65206e-756d62-657220-6f6620-736902-f59561-08d2f1-68912f-2f9a6a-a5c4c4-18E0E9"
+    * , "-s4",
+    * "bigintcs:007468-65206e-756d62-657220-6f6620-73697b-9e138b-9f851e-977897-c5dfab-4a33bd-03161E"
+    * , };
+    */
+
+   protected ArrayList<ShareData> caShrShareList = new ArrayList<ShareData>();
 
    @Override
    protected void onCreate( Bundle savedInstanceState ) {
       super.onCreate( savedInstanceState );
       setContentView( R.layout.activity_combine );
-            
+
       final Button btnAddShare = (Button)findViewById( R.id.btnAddShare );
       btnAddShare.setOnClickListener( new View.OnClickListener() {
-          public void onClick(View v) {
-             
-             final EditText txtShareNum =
-                (EditText)findViewById( R.id.txtShareNum );
-             final EditText txtShareString =
-                (EditText)findViewById( R.id.txtShareString );
-             try {
-                addShare(
-                   Integer.parseInt( txtShareNum.getText().toString() ),
-                   txtShareString.getText().toString()
-                );
-             } catch( NumberFormatException ex ) {
-                // TODO: Warn about using a numerical share ID.
-                return;
-             }
-             
-             txtShareNum.setText( "" );
-             txtShareString.setText( "" );
-          }
+         public void onClick( View v ) {
+
+            final EditText txtShareNum =
+               (EditText)findViewById( R.id.txtShareNum );
+            final EditText txtShareString =
+               (EditText)findViewById( R.id.txtShareString );
+            try {
+               addShare( Integer.parseInt( txtShareNum.getText().toString() ),
+                  txtShareString.getText().toString() );
+            } catch( NumberFormatException ex ) {
+               // TODO: Warn about using a numerical share ID.
+               return;
+            }
+
+            txtShareNum.setText( "" );
+            txtShareString.setText( "" );
+         }
       } );
-      
+
+      final Button btnScan = (Button)findViewById( R.id.btnScan );
+      btnScan.setOnClickListener( new View.OnClickListener() {
+
+         @Override
+         public void onClick( View v ) {
+            /* Intent intent = new Intent( "com.google.zxing.client.android.SCAN" );
+            intent.putExtra( "SCAN_MODE", "QR_CODE_MODE" );
+            startActivityForResult( intent, 0 ); */
+            IntentIntegrator itiScan = new IntentIntegrator( CombineActivity.this );
+            itiScan.initiateScan();
+         }
+      } );
+
       final Button btnCombine = (Button)findViewById( R.id.btnCombine );
       btnCombine.setOnClickListener( new View.OnClickListener() {
-          public void onClick(View v) {
-             combineTAShares( caShrShareList );
-          }
+         public void onClick( View v ) {
+            combineTAShares( caShrShareList );
+         }
       } );
 
       // Populate the share listbox.
       final ListView lstShares = (ListView)findViewById( R.id.lstShares );
-      ArrayAdapter<ShareData> adpShareAdapter = 
-         new ArrayAdapter<CombineActivity.ShareData>(
-            getApplicationContext(),
-            android.R.layout.simple_list_item_1,
-            caShrShareList
-         ) {
+      ArrayAdapter<ShareData> adpShareAdapter =
+         new ArrayAdapter<CombineActivity.ShareData>( getApplicationContext(),
+            android.R.layout.simple_list_item_1, caShrShareList ) {
 
             @Override
-            public View getView(
-               int intPositionIn,
-               View viwConvert,
-               ViewGroup vwgParent
-            ) {
-                View viwView = super.getView(
-                   intPositionIn, viwConvert, vwgParent
-                );
-                TextView txtView =
-                   (TextView)viwView.findViewById( android.R.id.text1 );
-                txtView.setTextColor( Color.BLACK );
-                return viwView;
+            public View getView( int intPositionIn, View viwConvert,
+               ViewGroup vwgParent ) {
+               View viwView =
+                  super.getView( intPositionIn, viwConvert, vwgParent );
+               TextView txtView =
+                  (TextView)viwView.findViewById( android.R.id.text1 );
+               txtView.setTextColor( Color.BLACK );
+               return viwView;
             }
          };
       lstShares.setAdapter( adpShareAdapter );
@@ -120,7 +129,27 @@ public class CombineActivity extends Activity {
       getMenuInflater().inflate( R.menu.combine, menu );
       return true;
    }
-   
+
+   @Override
+   public void onActivityResult( int intRequestCode, int intResultCode,
+      Intent intent ) {
+      if( 0 == intRequestCode ) {
+         if( RESULT_OK == intResultCode ) {
+            String strContents = intent.getStringExtra( "SCAN_RESULT" );
+            String strFormat = intent.getStringExtra( "SCAN_RESULT_FORMAT" );
+            // TODO: Handle successful scan.
+
+            // Place the returned code in the add share edit box.
+            final EditText txtShareString =
+               (EditText)findViewById( R.id.txtShareString );
+            txtShareString.setText( strContents );
+
+         } else if( RESULT_CANCELED == intResultCode ) {
+            // TODO: Handle cancel.
+         }
+      }
+   }
+
    @SuppressWarnings("unchecked")
    public void addShare( int intShareNumIn, String strShareStringIn ) {
       caShrShareList.add( new ShareData( intShareNumIn, strShareStringIn ) );
@@ -130,33 +159,33 @@ public class CombineActivity extends Activity {
 
    public String combineTAShares( ArrayList<ShareData> aShrShareDataIn ) {
       String strOutputSecret = null;
-      
+
       // Build a command line from the share list.
       ArrayList<String> aStrCommandLine = new ArrayList<String>();
       aStrCommandLine.add( "-k" );
       aStrCommandLine.add( "3" );
-      
+
       for( ShareData shrIter : aShrShareDataIn ) {
          aStrCommandLine.add( String.format( "-s%d", shrIter.cIntShareNum ) );
          aStrCommandLine.add( shrIter.cStrShareString );
       }
-      
+
       try {
          ByteArrayOutputStream osOutput = new ByteArrayOutputStream();
          PrintStream psOutput = new PrintStream( osOutput );
-         
+
          // Combine the shares and print them to a string.
          String[] aStrCommandLineRO = new String[aStrCommandLine.size()];
          aStrCommandLine.toArray( aStrCommandLineRO );
          CombineInput ssssInput = CombineInput.parse( aStrCommandLineRO );
          CombineOutput ssssOutput = ssssInput.output();
          ssssOutput.print( psOutput );
-         
-         // We could conceivably use subclassing if the secret hadn't been 
+
+         // We could conceivably use subclassing if the secret hadn't been
          // marked "private". There might be a better way to do this but
-         // quick and dirty should work for now. Feel free to submit 
+         // quick and dirty should work for now. Feel free to submit
          // improvements!
-         String strOutput = new String( osOutput.toByteArray(),"UTF-8" );
+         String strOutput = new String( osOutput.toByteArray(), "UTF-8" );
          // Toast.makeText( this, strOutput, Toast.LENGTH_LONG ).show();
          Pattern ptnOutput = Pattern.compile( "secret.string = '(.+?)'" );
          Matcher mtcOutput = ptnOutput.matcher( strOutput );
@@ -165,7 +194,7 @@ public class CombineActivity extends Activity {
          } else {
             throw new SecretShareException( "Output parsing failed." );
          }
-         
+
          // Toast.makeText( this, strOutputSecret, Toast.LENGTH_LONG ).show();
       } catch( Exception ex ) {
          Toast.makeText( this, ex.getMessage(), Toast.LENGTH_LONG ).show();
